6cf1c5ae892a976223b9ada6b77c2e5e8723d247
naveenkmedi <naveenk.medi@gmail.com>
2018-12-13 07:53:05 +0530

    binary tree

--- makefile
+++ makefile
@@ -1,12 +1,13 @@
 $build:
+	g++ src/bTree.cpp -o t.elf
 	# g++ src/wait.cpp -o w.elf
 	# g++ src/bin.cpp -o b.elf
 	# g++ src/floor.cpp -o f.elf
 	# gcc src/lList.c -o l.elf
 	# gcc src/insertion.c -o i.elf
-	g++ src/mergeS.cpp -o m.elf
+	# g++ src/mergeS.cpp -o m.elf
 	# gcc src/sort.c -o s.elf
 	# g++ src/find_.cpp -o f_.elf
-	g++ src/firstR_.cpp -o fr.elf
-	g++ src/quick.cpp -o q.elf
-	g++ src/dutchF.cpp -o d.elf
+	# g++ src/firstR_.cpp -o fr.elf
+	# g++ src/quick.cpp -o q.elf
+	# g++ src/dutchF.cpp -o d.elf
---
+++ src/bTree.cpp
@@ -0,0 +1,62 @@
+//print a binary tree
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+typedef struct bTree{
+	int data;
+	bTree* left;
+	bTree* right;
+}bTree;
+
+void printTree(bTree* head)
+{
+	if(head != NULL)
+	{
+		cout<<head->data<<" ";
+		// usleep(100000);
+		printTree(head->left);
+		printTree(head->right);
+	}
+}
+
+int main()
+{
+	bTree* head = new bTree();
+	bTree* node1 = new bTree();
+	bTree* node2 = new bTree();
+	bTree* node3 = new bTree();
+	bTree* node4 = new bTree();
+	bTree* node5 = new bTree();
+	bTree* node6 = new bTree();
+
+	head->data = 5;
+	node1->data = 10;
+	node2->data = 20;
+	node3->data = 30;
+	node4->data = 40;
+	node5->data = 50;
+	node6->data = 60;
+
+	head->left = node1;
+	head->right = node2;
+	node1->left = node3;
+	node1->right = node4;
+	node2->left = node5;
+	node2->right = node6;
+
+	node3->left = NULL;
+	node4->left = NULL;
+	node5->left = NULL;
+	node6->left = NULL;
+
+	node3->right = NULL;
+	node4->right = NULL;
+	node5->right = NULL;
+	node6->right = NULL;
+
+	printTree(head);
+	cout<<"\n";
+
+	return 0;
+}
